326a408cec1d89b50f95d7adb737d7feb8481e62
gilliam.zhao <gilliam.zhao@dianping.com>
2018-08-31 16:48:33 +0800

    创建一个单例对象

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 const Vue = require('vue')
 const server = require('express')()
-const renderer = require('vue-server-renderer').createRenderer()
+// const renderer = require('vue-server-renderer').createRenderer()
+
+const renderer = require('vue-server-renderer').createRenderer({
+  template: require('fs').readFileSync('./index.template.html', 'utf-8')
+})
+
+let count = 0
 
 server.get('*', (req, res) => {
   const app = new Vue({
     data: {
-      url: req.url
+      url: req.url,
+      count: count++
     },
-    template: `<div>访问的 URL 是：{{ url }}</div>`
+    template: `<div>访问的 URL 是：{{ url }} {{ count }}</div>`
   })
+
+  const context = {
+    title: 'Hello Gil',
+    meta: ``
+  }
   
-  renderer.renderToString(app, (err, html) => {
+  renderer.renderToString(app, context, (err, html) => {
     if (err) {
       res.status(500).end('Internal Server Error')
       return
     }
-    res.end(`
-      <!DOCTYPE html>
-      <html lang="en" >
-        <head>
-          <meta charset="utf-8">
-          <title>Hello</title>
-        </head>
-        <body>${html}</body>
-      </html>
-    `)
+    res.end(`${ html }`)
   }) 
 })
 
