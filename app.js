74642674047c5612c5ab7ca36a292b88503a21fa
gilliam.zhao <gilliam.zhao@dianping.com>
2018-08-31 17:05:44 +0800

    为每个请求创建新的应用实例

--- app.js
+++ app.js
@@ -1,34 +1,12 @@
 const Vue = require('vue')
-const server = require('express')()
-// const renderer = require('vue-server-renderer').createRenderer()
 
-const renderer = require('vue-server-renderer').createRenderer({
-  template: require('fs').readFileSync('./index.template.html', 'utf-8')
-})
-
-let count = 0
-
-server.get('*', (req, res) => {
-  const app = new Vue({
+module.exports = function createApp (context) {
+  let count = 0
+  return new Vue({
     data: {
-      url: req.url,
+      url: context.url,
       count: count++
     },
     template: `<div>访问的 URL 是：{{ url }} {{ count }}</div>`
   })
-
-  const context = {
-    title: 'Hello Gil',
-    meta: ``
-  }
-  
-  renderer.renderToString(app, context, (err, html) => {
-    if (err) {
-      res.status(500).end('Internal Server Error')
-      return
-    }
-    res.end(`${ html }`)
-  }) 
-})
-
-server.listen(8000)
+}
