fc2d3ddf3c9fa262e4ac0d976590fd4288947dee
gilliam.zhao <gilliam.zhao@dianping.com>
2018-09-04 18:04:39 +0800

    upt

--- src/app.vue
+++ src/app.vue
@@ -1,19 +1,21 @@
 <template>
-  <div>{{ text }}</div>
+  <video id="video" preload="none" poster="https://media.w3.org/2010/05/sintel/poster.png">
+    <source id="mp4" src="https://media.w3.org/2010/05/sintel/trailer.mp4" type="video/mp4">
+    <p>Your user agent does not support the HTML5 Video element.</p>
+  </video>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      text: 'abc'
-    }
-  }
-}
+	data() {
+		return {
+		};
+	},
+};
 </script>
 
 <style lang="less">
 div {
-  color: blueviolet;
+	color: blueviolet;
 }
 </style>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
 import App from './app.vue'
 
-import './assets/styles/less.less'
-import './assets/images/bg-head.png'
-
 const root = document.createElement('div')
 document.body.appendChild(root)
 
